ecde912ae5e6d3521fb6d71390c41719f184c034
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-13 18:47:10 -0400

    Create controller actions for user#new, user#create, and user#show. Add before_action filters to find and ensure user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:new, :create]
+  before_action :find_and_ensure_user, except: [:new, :create]
   def new
     @user = User.new
   end
 
   def create
+    binding.pry
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
@@ -17,16 +19,6 @@ class UsersController < ApplicationController
   end
 
   def show
-
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 
   private
@@ -35,4 +27,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :position, :password, :password_confirmation)
   end
 
+  def find_and_ensure_user
+    render 'application/error404' unless @user = User.find_by(id: session[:user_id])
+  end
 end
