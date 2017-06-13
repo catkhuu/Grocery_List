079337f830bef9eb70076fa971592509f6f492fb
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-13 18:48:41 -0400

    Create methods to authenticate user, set current user, and ensure a user is logged in for SessionsHelper.

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,15 @@
+module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def authenticate_user!
+    unless logged_in?
+      redirect_to new_session_path
+    end
+  end
+end
