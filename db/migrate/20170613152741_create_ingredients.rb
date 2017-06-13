689987a09eb41960e77754b912149f87ab47a978
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-13 11:32:08 -0400

    Add datatypes and columns to Ingredient.

--- db/migrate/20170613152741_create_ingredients.rb
+++ db/migrate/20170613152741_create_ingredients.rb
@@ -1,6 +1,9 @@
 class CreateIngredients < ActiveRecord::Migration[5.1]
   def change
     create_table :ingredients do |t|
+      t.string  :name, null: false
+      t.boolean :is_specialty, default: false
+      t.timestamps null: false
     end
   end
 end
