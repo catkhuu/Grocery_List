4b3bcff5c3255c11c342644d39b762abe6cd7c50
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-13 18:44:32 -0400

    Extracted methods into SessionsHelper and included in application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
+  include SessionsHelper
   protected
-
-  def current_user
-    @current_user ||= User.find_by(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !current_user.nil?
-  end
-
-  def authenticate_user!
-    unless logged_in?
-      redirect_to new_session_path
-    end
-  end
 end
