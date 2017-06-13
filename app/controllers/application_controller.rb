7ada7eda5e119a240d7d2a6fb0d1d574dc9cd3bc
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-13 16:21:57 -0400

    Add before_action filter to authenticate user. Add sessions helpers methods to application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  protected
+
+  def current_user
+    @current_user ||= User.find_by(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def authenticate_user!
+    unless logged_in?
+      redirect_to new_session_path
+    end
+  end
 end
