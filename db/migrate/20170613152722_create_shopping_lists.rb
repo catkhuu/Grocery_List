e6696d8092f3d045943de57fbe399a1a9f8b827c
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-13 11:31:14 -0400

    Add datatypes and columns to ShoppingList.

--- db/migrate/20170613152722_create_shopping_lists.rb
+++ db/migrate/20170613152722_create_shopping_lists.rb
@@ -1,6 +1,10 @@
 class CreateShoppingLists < ActiveRecord::Migration[5.1]
   def change
     create_table :shopping_lists do |t|
+      t.string  :title, default: 'Untitled'
+      t.datetime  :shopping_date
+      t.references :user, index: true, foreign_key: true
+      t.timestamps null: false
     end
   end
 end
