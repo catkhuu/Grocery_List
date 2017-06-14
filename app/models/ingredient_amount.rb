5d009bab2f7be410d94679a2b08100502689b1f7
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-14 15:18:41 -0400

    Add validations for IngredientAmount.

--- app/models/ingredient_amount.rb
+++ app/models/ingredient_amount.rb
@@ -1,4 +1,6 @@
 class IngredientAmount < ApplicationRecord
   belongs_to :test_recipe
   belongs_to :ingredient
+
+  validates :units, :ingredient_amount, :test_recipe_id, :ingredient_id, presence: true
 end
