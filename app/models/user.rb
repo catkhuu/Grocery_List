3c9cf9329495edf2567e487a9030fa0ab6bb77bf
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-14 15:05:54 -0400

    Add validations for User attributes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :shopping_lists, dependent: :destroy
   has_many :test_recipes, through: :shopping_lists
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, uniqueness: true, presence: true
+  validates :username, length: { minimum: 6, maximum: 20 }, presence: true
+  validates :position, presence: true
+  validates :password, length: { minimum: 8, maximum: 20 }
 end
