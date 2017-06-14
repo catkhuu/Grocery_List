61a4bb50887949f51d51ed5cd25140400d98501d
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-14 15:57:00 -0400

    Add presence: true to validation.

--- app/models/shopping_list_test_recipe.rb
+++ app/models/shopping_list_test_recipe.rb
@@ -2,5 +2,5 @@ class ShoppingListTestRecipe < ApplicationRecord
   belongs_to :shopping_list
   belongs_to :test_recipe
 
-  validates :shopping_list_id, :test_recipe_id 
+  validates :shopping_list_id, :test_recipe_id, presence: true
 end
