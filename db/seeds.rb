827e38c40ca48467e2cbd67ee155bef8c15839da
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-15 19:35:38 -0400

    Edit seed data to reflect ingredient_amount unit options in IngredientAmount controller. Capitalize all ingredient names.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,31 +13,31 @@ summer_grillers = ShoppingList.create!(title: 'Summer Grill Specials', shopping_
 
 
 # Create Ingredients
-garlic = Ingredient.create!(name: 'garlic')
-red_onion = Ingredient.create!(name: 'red onion')
-watermelon = Ingredient.create!(name: 'watermelon', is_specialty: true)
-champagne_mango = Ingredient.create!(name: 'champagne mango', is_specialty: true)
-romaine = Ingredient.create!(name: 'romaine')
-dandelion_greens = Ingredient.create!(name: 'dandelion greens', is_specialty: true)
-butter = Ingredient.create!(name: 'butter')
-whole_oats = Ingredient.create!(name: 'whole oats')
-shiitake_mushrooms = Ingredient.create!(name: 'shiitake mushrooms', is_specialty: true)
-miso_paste = Ingredient.create!(name: 'miso paste')
-pasta = Ingredient.create!(name: 'pasta', is_specialty: true)
-roma_tomato = Ingredient.create!(name: 'roma tomato')
-basil = Ingredient.create!(name: 'basil')
-olive_oil = Ingredient.create!(name: 'olive oil')
-saffron = Ingredient.create!(name: 'saffron', is_specialty: true)
-black_garlic = Ingredient.create!(name: 'black garlic', is_specialty: true)
-quinoa = Ingredient.create!(name: 'quinoa')
-curry_powder = Ingredient.create!(name: 'curry powder')
-red_lentils = Ingredient.create!(name: 'red lentils')
-garbanzo_beans = Ingredient.create!(name: 'garbanzo beans')
-tumeric = Ingredient.create!(name: 'tumeric')
-cayenne = Ingredient.create!(name: 'cayenne')
-sea_salt = Ingredient.create!(name: 'sea salt')
-black_salt = Ingredient.create!(name: 'black salt', is_specialty: true)
-vegetable_stock = Ingredient.create!(name: 'vegetable stock')
+garlic = Ingredient.create!(name: 'Garlic')
+red_onion = Ingredient.create!(name: 'Red Onion')
+watermelon = Ingredient.create!(name: 'Watermelon', is_specialty: true)
+champagne_mango = Ingredient.create!(name: 'Champagne Mango', is_specialty: true)
+romaine = Ingredient.create!(name: 'Romaine')
+dandelion_greens = Ingredient.create!(name: 'Dandelion Greens', is_specialty: true)
+butter = Ingredient.create!(name: 'Butter')
+whole_oats = Ingredient.create!(name: 'Whole Oats')
+shiitake_mushrooms = Ingredient.create!(name: 'Shiitake Mushrooms', is_specialty: true)
+miso_paste = Ingredient.create!(name: 'Miso Paste')
+pasta = Ingredient.create!(name: 'Pasta', is_specialty: true)
+roma_tomato = Ingredient.create!(name: 'Roma Tomato')
+basil = Ingredient.create!(name: 'Basil')
+olive_oil = Ingredient.create!(name: 'Olive Oil')
+saffron = Ingredient.create!(name: 'Saffron', is_specialty: true)
+black_garlic = Ingredient.create!(name: 'Black Garlic', is_specialty: true)
+quinoa = Ingredient.create!(name: 'Quinoa')
+curry_powder = Ingredient.create!(name: 'Curry Powder')
+red_lentils = Ingredient.create!(name: 'Red Lentils')
+garbanzo_beans = Ingredient.create!(name: 'Garbanzo Beans')
+tumeric = Ingredient.create!(name: 'Tumeric')
+cayenne = Ingredient.create!(name: 'Cayenne')
+sea_salt = Ingredient.create!(name: 'Sea Salt')
+black_salt = Ingredient.create!(name: 'Black salt', is_specialty: true)
+vegetable_stock = Ingredient.create!(name: 'Vegetable Stock')
 
 
 # Create note for specialty ingredients
@@ -51,48 +51,48 @@ specialty_dandelion_greens_2 = SpecialtyPurchaseDestination.create!(company_name
 
 
 # Create Test Recipes
-simple_tomato_basil_pasta = TestRecipe.create!(title: 'simple tomato basil pasta')
+simple_tomato_basil_pasta = TestRecipe.create!(title: 'Simple tomato basil pasta')
 
-curried_lentil_stew = TestRecipe.create!(title: 'curried lentil stew')
+curried_lentil_stew = TestRecipe.create!(title: 'Curried lentil stew')
 
-miso_glazed_dandelion_salad = TestRecipe.create!(title: 'miso glazed dandelion salad')
+miso_glazed_dandelion_salad = TestRecipe.create!(title: 'Miso glazed dandelion salad')
 
 
 # Create Ingredient Amounts
   ## Ingredient Amounts for Recipe #1
-    amount_pasta = IngredientAmount.create!(amount: 8, units: 'oz', test_recipe: simple_tomato_basil_pasta, ingredient: pasta)
+    amount_pasta = IngredientAmount.create!(amount: 60, units: 'Grams', test_recipe: simple_tomato_basil_pasta, ingredient: pasta)
 
-    amount_roma_tomato = IngredientAmount.create!(amount: 2, units: 'lbs', test_recipe: simple_tomato_basil_pasta, ingredient: roma_tomato)
+    amount_roma_tomato = IngredientAmount.create!(amount: 8, units: 'Each', test_recipe: simple_tomato_basil_pasta, ingredient: roma_tomato)
 
-    amount_basil = IngredientAmount.create!(amount: 4, units: 'bunch', test_recipe: simple_tomato_basil_pasta, ingredient: basil)
+    amount_basil = IngredientAmount.create!(amount: 2, units: 'Bunch', test_recipe: simple_tomato_basil_pasta, ingredient: basil)
 
-    amount_olive_oil = IngredientAmount.create!(amount: 2, units: 'tbsp', test_recipe: simple_tomato_basil_pasta, ingredient: olive_oil)
+    amount_olive_oil = IngredientAmount.create!(amount: 2, units: 'Tbsp', test_recipe: simple_tomato_basil_pasta, ingredient: olive_oil)
 
-    amount_sea_salt = IngredientAmount.create!(amount: 1, units: 'tsp', test_recipe: simple_tomato_basil_pasta, ingredient: sea_salt)
+    amount_sea_salt = IngredientAmount.create!(amount: 1, units: 'Tsp', test_recipe: simple_tomato_basil_pasta, ingredient: sea_salt)
 
-    amount_garlic = IngredientAmount.create!(amount: 4, units: 'cloves', test_recipe: simple_tomato_basil_pasta, ingredient: garlic)
+    amount_garlic = IngredientAmount.create!(amount: 4, units: 'Each', test_recipe: simple_tomato_basil_pasta, ingredient: garlic)
 
   ## Ingredient Amounts for Recipe #2
-    amount_curry_powder = IngredientAmount.create!(amount: 1, units: 'tbsp', test_recipe: curried_lentil_stew, ingredient: curry_powder)
+    amount_curry_powder = IngredientAmount.create!(amount: 1, units: 'Tbsp', test_recipe: curried_lentil_stew, ingredient: curry_powder)
 
-    amount_garlic_curry = IngredientAmount.create!(amount: 6, units: 'cloves', test_recipe: curried_lentil_stew, ingredient: garlic)
+    amount_garlic_curry = IngredientAmount.create!(amount: 6, units: 'Each', test_recipe: curried_lentil_stew, ingredient: garlic)
 
-    amount_roma_tomato_curry = IngredientAmount.create!(amount: 1, units: 'lbs', test_recipe: curried_lentil_stew, ingredient: roma_tomato)
+    amount_roma_tomato_curry = IngredientAmount.create!(amount: 3, units: 'Each', test_recipe: curried_lentil_stew, ingredient: roma_tomato)
 
-    amount_lentils = IngredientAmount.create!(amount: 2, units: 'cups', test_recipe: curried_lentil_stew, ingredient: red_lentils)
+    amount_lentils = IngredientAmount.create!(amount: 100, units: 'Grams', test_recipe: curried_lentil_stew, ingredient: red_lentils)
 
-    amount_vegetable_stock = IngredientAmount.create!(amount: 6, units: 'cups', test_recipe: curried_lentil_stew, ingredient: vegetable_stock)
+    amount_vegetable_stock = IngredientAmount.create!(amount: 907, units: 'Grams', test_recipe: curried_lentil_stew, ingredient: vegetable_stock)
 
   ## Ingredient Amounts for Recipe #3
-    amount_miso_paste = IngredientAmount.create!(amount: 3, units: 'tbsp', test_recipe: miso_glazed_dandelion_salad, ingredient: miso_paste)
+    amount_miso_paste = IngredientAmount.create!(amount: 3, units: 'Tbsp', test_recipe: miso_glazed_dandelion_salad, ingredient: miso_paste)
 
-    amount_dandelion_greens = IngredientAmount.create!(amount: 1, units: 'bunch', test_recipe: miso_glazed_dandelion_salad, ingredient: dandelion_greens)
+    amount_dandelion_greens = IngredientAmount.create!(amount: 1, units: 'Bunch', test_recipe: miso_glazed_dandelion_salad, ingredient: dandelion_greens)
 
-    amount_romaine = IngredientAmount.create!(amount: 1, units: 'bunch', test_recipe: miso_glazed_dandelion_salad, ingredient: romaine)
+    amount_romaine = IngredientAmount.create!(amount: 1, units: 'Bunch', test_recipe: miso_glazed_dandelion_salad, ingredient: romaine)
 
-    amount_black_salt = IngredientAmount.create!(amount: 1, units: 'tsp', test_recipe: miso_glazed_dandelion_salad, ingredient: black_salt)
+    amount_black_salt = IngredientAmount.create!(amount: 1, units: 'Tsp', test_recipe: miso_glazed_dandelion_salad, ingredient: black_salt)
 
-    amount_roma_tomato_miso = IngredientAmount.create!(amount: 1, units: 'lbs', test_recipe: miso_glazed_dandelion_salad, ingredient: roma_tomato)
+    amount_roma_tomato_miso = IngredientAmount.create!(amount: 3, units: 'Each', test_recipe: miso_glazed_dandelion_salad, ingredient: roma_tomato)
 
 
 # Add Test Recipes to Shopping Lists
