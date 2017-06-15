894509aaab8a7106dc4475848e5e5562ae8c92c0
Catherine Khuu <catherine.khuu-i@plated.com>
2017-06-15 09:59:21 -0400

    Test UsersController and SessionsController. Remove binding.pry. Create ShoppingList#display_shopping_date method to pretty print/convert date. Display upcoming shopping trips on user#show. In process -- convert nav bar to slide out nav.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,21 @@
 //= require_tree .
 //= require materialize
 //= require materialize/extras/nouislider
+
+$(document).ready(function(){
+  alert('ready');
+
+  $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+      }
+    );
+  $('.button-collapse').sideNav('show');
+  $('.button-collapse').sideNav('hide');
+  $('.slider').slider({full_width: true});
+
+  $('select').material_select();
+
+})
